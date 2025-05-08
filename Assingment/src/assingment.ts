308f2c454ae0414b6472482c94e240c0de4650fe
Moinuddin-dotcom <moinchy7@gmail.com>
2025-05-08 14:46:38 +0600

    problem 2 complete

--- Assingment/src/assingment.ts
+++ Assingment/src/assingment.ts
@@ -11,3 +11,18 @@
 
 // formatString("hello world", false);
 
+
+// Problem 2
+
+// const books = [
+//     { title: "Book A", rating: 4.5 },
+//     { title: "Book B", rating: 3.2 },
+//     { title: "Book C", rating: 5.0 }
+// ];
+
+// function filterByRating(items: { title: string; rating: number }[]): { title: string; rating: number }[] {
+//     const res = items.filter(item => item.rating >= 4);
+//     return res
+// }
+
+// console.log(filterByRating(books))
