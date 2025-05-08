5fc23ec7ffe02f7d215144bd247b74f767ff50dc
Moinuddin-dotcom <moinchy7@gmail.com>
2025-05-08 14:45:27 +0600

    first problem done

---
+++ Assingment/src/assingment.ts
@@ -0,0 +1,13 @@
+// Problem 1
+// function formatString(input: string, toUpper?: boolean): string {
+//     if (toUpper === true || toUpper === undefined) {
+//         console.log(input.toUpperCase())
+//         return input.toUpperCase();
+//     } else {
+//         console.log(input.toLowerCase())
+//         return input.toLowerCase();
+//     }
+// }
+
+// formatString("hello world", false);
+
